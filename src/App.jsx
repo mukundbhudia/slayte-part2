e557d04aa23ffc3889226ddeb4555b3f298151f6
Mukund Bhudia <mukundbhudia@users.noreply.github.com>
2020-08-26 14:51:23 +0100

    Removed unused components A compositional approach as apposed to inheritance

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import "./styles.css";
-
+// Just one component but passing in different props
 import Fetch from "./components/Fetch";
-import FetchStringify from "./components/FetchStringify";
-import FetchBase64Hash from "./components/FetchBase64Hash";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Slayte challenge part 2</h1>
-      <Fetch src={"https://air-qual-api.herokuapp.com/"} />
+      <Fetch src="https://air-qual-api.herokuapp.com/" />
       <br />
       <br />
-      <FetchStringify
-        src={"https://air-qual-api.herokuapp.com/getStation/?stnId=5724"}
+      <Fetch
+        src="https://air-qual-api.herokuapp.com/getStation/?stnId=5724"
         depth={1}
       />
       <br />
       <br />
-      <FetchBase64Hash
-        src={"https://air-qual-api.herokuapp.com/getStation/?stnId=9974"}
+      <Fetch
+        src="https://air-qual-api.herokuapp.com/getStation/?stnId=9974"
+        encoding="base64"
       />
     </div>
   );
